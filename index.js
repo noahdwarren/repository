776d0a72a87102106535b60f0431a1c6a019c256
Noah <noah.d.warren@gmail.com>
2021-10-29 14:00:43 -0500

    added some tests

---
+++ index.js
@@ -0,0 +1,17 @@
+var numberOfLights = 150;
+var Name = 'sun';
+var isColdOutside = true;
+var isHappy = false;
+var numberOfFriends = 12
+var x = 8;
+var y = 12;
+var z = x + y;
+
+
+console.log('Hello World');
+console.log(5 + 3);
+console.log(isColdOutside);
+console.log(isHappy);
+console.log(x = y + x)
+console.log(z);
+
---
+++ test.js
@@ -0,0 +1,27 @@
+let firstName = 'Noah';
+let middleInitial = 'D';
+let lastName = 'Warren';
+
+let fullName = firstName + ' ' + middleInitial + ' ' + lastName
+console.log(fullName);
+
+let bankAccountBalance = 800
+
+bankAccountBalance = bankAccountBalance - 339
+
+bankAccountBalance = bankAccountBalance + 83
+
+console.log(bankAccountBalance);
+
+let isHotOutside = false;
+
+isHotOutside = !isHotOutside
+
+console.log(isHotOutside);
+
+var x = 40
+var y = 28
+var z = x * y
+
+console.log(z)
+
